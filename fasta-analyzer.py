55ff9c95e0444d53cea02b7b2f4671c106046e92
Sandra Karlsten <sandra.karlsten@gmail.com>
2014-07-08 10:07:22 +0200

    Moved out print_output and read_file from the class.

--- fasta-analyzer.py
+++ fasta-analyzer.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import argparse
 import sys
 from matplotlib import pyplot as plt
 
 
+
 # This code is partly based on https://github.com/mtop/ngs/blob/master/fp.py by Mats Töpel
+
 
 
 parser = argparse.ArgumentParser(description = 
@@ -43,24 +44,13 @@ args = parser.parse_args()
 
 
 
+
 # Each sequence becomes one object of the class Fasta.
 class Fasta(object):
 
-    def __init__(self):
-        pass
-
-    # Read fasta file.
-    def read_file(self, filename):
-        filename.seek(0)
-        self.name, self.seq = None, []
-        for line in filename:
-            if line.startswith('>'):
-                if self.name:
-                    yield (self.name, ''.join(self.seq))
-                self.name, self.seq = line, []
-            else:
-                self.seq.append(line)
-        if self.name: yield (self.name, ''.join(self.seq))
+    def __init__(self, name, seq):
+        self.name = name
+        self.seq = seq
 
     def header(self):
         return self.name[1:].rstrip()
@@ -69,46 +59,70 @@ class Fasta(object):
         return self.seq
 
     def length(self):
-        self.seq = str(self.seq)
         return len(self.seq)
 
     # Calculates the number of G and C relative to the 
     # total number of G, C, A and T.
     def gccount(self):
-        self.seq = str(self.seq)
         self.gc = self.seq.count("G") + self.seq.count("C")
         self.total = self.seq.count("G") + self.seq.count("C") + \
                      self.seq.count("A") + self.seq.count("T")
         self.content = (float(self.gc) / self.total) * 100
         return self.content
 
-    # Print the outputs that were chosen and save the gc content 
-    # and length in lists.
-    def output(self):
-        dictionary = {}
-        for line in self.read_file(args.infile):
-            dict2 = {self.header() : self}
-            dictionary.update(dict2)
-            if args.header == True:
-                print self.header(), '\t',
-            if args.length == True:
-                print dictionary[self.header()].length(), '\t',
-            if args.gccontent == True:
-                print dictionary[self.header()].gccount(),
-            if len(sys.argv) > 2:    # If no flags are given, 
-                                     # no line breaks are printed.
-                print	# Just there to introduce a line break.
-            if args.plot == True:
-                plt.scatter(dictionary[self.header()].length(), 
-                dictionary[self.header()].gccount())
-                plt.ylabel('GC content (%)')
-                plt.xlabel('Length (nt)')
-        plt.show()
-        args.infile.close()
+
+
+
+
+# Read fasta file.
+def read_file(infile):
+    args.infile.seek(0)
+    name, seq = None, []
+    for line in args.infile:
+        if line.startswith('>'):
+            if name:
+                yield (name, ''.join(seq))
+            name, seq = line, []
+        else:
+            seq.append(line)
+    if name: yield (name, ''.join(seq))
+
+
+
+
+
+# Print the outputs and plot GC content against length if the
+# flags are given.
+def print_output():
+    for name, seq in read_file(args.infile):
+        fs = Fasta(name, seq)
+        dict2 = {fs.header() : fs}
+        dictionary.update(dict2)
+        if args.header == True:
+            print fs.header(), '\t',
+        if args.length == True:
+            print dictionary[fs.header()].length(), '\t',
+        if args.gccontent == True:
+            print dictionary[fs.header()].gccount(),
+        if len(sys.argv) > 2:    # If no flags are given, 
+                                 # no line breaks are printed.
+            print	# Just there to introduce a line break.
+        if args.plot == True:
+            plt.scatter(dictionary[fs.header()].length(), dictionary[fs.header()].gccount())
+            plt.ylabel('GC content (%)')
+            plt.xlabel('Length (nt)')
+    plt.show()
+    args.infile.close()
+
+
+
+
+
+dictionary = {}
+
 
 
 
 
 if __name__ == "__main__":
-    Fasta().output()
-
+    print_output()
